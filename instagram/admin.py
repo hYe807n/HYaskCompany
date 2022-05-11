e17a318c69d594cb75dfd2691dc9a50263173cd0
hYe807n <rlagkdus55@gmail.com>
2022-05-11 23:33:25 +0900

    commit

---
+++ instagram/admin.py
@@ -0,0 +1,23 @@
+from csv import list_dialects
+from django.contrib import admin
+from jmespath import search
+from .models import Post
+from django.utils.safestring import mark_safe
+# Register your models here.
+
+#admin.site.register(Post) 
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id','photo_tag', 'message','message_length','is_public', 'created_at', 'updated_at']
+    list_display_links = ['message']
+    list_filter = ['created_at','is_public']
+    search_fields = ['message']
+
+    def photo_tag(self, post):
+        if post.photo:
+            return mark_safe(f'<img src="{post.photo.url}"style="width: 70px;" />' )
+        return None
+#admin.site.register(Post, PostAdmin)
+    def message_length(self, post):
+        return " 글자"
